5d239062c1893f7bacbf6976e8822b3fbfcdd58d
taswar <taswar@zeytinsoft.com>
2013-03-08 22:02:35 -0500

    Demo code

---
+++ demo/Controllers/ProductsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using demo.Models;
+
+namespace demo.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private ProductContext db = new ProductContext();
+
+        // GET api/Products
+        [Queryable(PageSize = 1)]
+        public IEnumerable<Product> GetProducts()
+        {
+            return db.Products.AsEnumerable();
+        }
+
+        // GET api/Products/5
+        public Product GetProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return product;
+        }
+
+        // PUT api/Products/5
+        public HttpResponseMessage PutProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != product.ProductId)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/Products
+        public HttpResponseMessage PostProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, product);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = product.ProductId }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Products/5
+        public HttpResponseMessage DeleteProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Products.Remove(product);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ demo/Formatter/CsvFormatter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using demo.Models;
+
+namespace demo.Formatter
+{
+    public class CsvFormatter : BufferedMediaTypeFormatter 
+    {
+        public CsvFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/csv"));
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            return false;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            if (type == typeof(Product))
+            {
+                return true;
+            }
+            
+            Type enumerableType = typeof(IEnumerable<Product>);
+            return enumerableType.IsAssignableFrom(type);
+        }
+
+        
+        public override void WriteToStream(Type type, object value, Stream writeStream, System.Net.Http.HttpContent content)
+        {
+            using (var writer = new StreamWriter(writeStream))
+            {
+
+                var products = value as IEnumerable<Product>;
+                if (products != null)
+                {
+                    foreach (var product in products)
+                    {
+                        WriteItem(product, writer);
+                    }
+                }
+                else
+                {
+                    var singleProduct = value as Product;
+                    if (singleProduct == null)
+                    {
+                        throw new InvalidOperationException("Cannot serialize type");
+                    }
+                    WriteItem(singleProduct, writer);
+                }
+            }
+            writeStream.Close();
+        }
+
+        // Helper methods for serializing Products to CSV format. 
+        private void WriteItem(Product product, StreamWriter writer)
+        {
+            writer.WriteLine("{0},{1}", Escape(product.ProductId), Escape(product.ProductName));
+        }
+
+        static readonly char[] _specialChars = new char[] { ',', '\n', '\r', '"' };
+
+        private string Escape(object o)
+        {
+            if (o == null)
+            {
+                return "";
+            }
+
+            string field = o.ToString();
+
+            return field.IndexOfAny(_specialChars) != -1 ? String.Format("\"{0}\"", field.Replace("\"", "\"\"")) : field;
+        }
+    }
+}
